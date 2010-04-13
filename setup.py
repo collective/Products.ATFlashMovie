262574d05582f2fef415e1fa9d5ec1ceadb7c9e8
deo <deo@localhost>
2010-04-13 20:27:05 +0000

    Added basic eggfied structure.
    svn path=/Products.ATFlashMovie/trunk/; revision=115271

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = read('Products', 'ATFlashMovie', 'version.txt').strip()
+
+setup(name='Product.ATFlashMovie',
+      version=version,
+      description='A Flash content-type and extra views for Plone.',
+      long_description=read('Products', 'ATFlashMovie', 'README.txt') + "\n\n" +
+                       read('Products', 'ATFlashMovie', 'CHANGES.txt'),
+      classifiers=[
+          'Programming Language :: Python',
+          'Framework :: Plone',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+      ],
+      keywords='plone flash',
+      author='Jean-Charles Rogez',
+      author_email='jeancharles.rogez@free.fr',
+      url='http://plone.org/products/atflashmovie',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      )
